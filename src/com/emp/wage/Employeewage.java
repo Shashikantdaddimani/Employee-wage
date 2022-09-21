e6cd7b891d74718e2f50da82638ce87e42cf9e86
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-09-21 13:22:45 +0530

    Added Welcome message

---
+++ src/com/emp/wage/Employeewage.java
@@ -0,0 +1,10 @@
+package com.emp.wage;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+	}
+
+}
